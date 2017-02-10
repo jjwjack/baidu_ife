afc5f1869f786b387eeb2f5b1dbe91bee0d385fb
jjwjack <jjwjack@163.com>
2017-02-11 00:56:13 +0800

    task-16，获取输入的值并在表格显示。

--- task-16/main.js
+++ task-16/main.js
@@ -7,7 +7,7 @@
 * };
 */
 var aqiData = {};
-
+var btn_add = document.getElementById("btn_add");
 /**
 * 从用户输入中获取数据，向aqiData中增加一条数据
 * 然后渲染aqi-list列表，增加新增的数据
@@ -15,35 +15,36 @@ var aqiData = {};
 function addAqiData() {
 	var input_city = document.getElementById("aqi-city-input").value;
 	var input_value = document.getElementById("aqi-value-input").value;
-	aqiData['"'+input_city+'"'] = input_value;
-	// aqiData.push({input_city,input_value});
+	aqiData[input_city] = input_value;
 	return aqiData;
-	// console.log(aqiData);
 }
-var btn_add = document.getElementById("btn_add");
-btn_add.onclick = function(){
-	addAqiData();
-};
 
 /**
 * 渲染aqi-table表格
 */
 function renderAqiList() {
 	var aqi_table = document.getElementById("aqi-table");
-	var aqi_th = document.createElement("th");
+	var aqi_th = document.createElement("tr");
 	// <td>城市</td><td>空气质量</td><td>操作</td>
-	aqi_th.innerHTML = "<td>城市</td><td>空气质量</td><td>操作</td>";
+	aqi_th.innerHTML = "<th>城市</th><th>空气质量</th><th>操作</th>";
 	aqi_table.appendChild(aqi_th);
-	for (var i = 0; i < aqiData.length; i++) {
+	//用for in遍历json对象
+	for (var key in aqiData){
+		// console.log(key);
+		// console.log(aqiData[key]);
 		var aqi_tr = document.createElement("tr");
 		// <td>北京</td><td>90</td><td><button>删除</button></td>
-		aqi_tr.innerHTML = "<td>"+aqiData[i][0]+"</td><td>"+aqiData[i][1]+"</td><td><button>删除</button></td>";
+		aqi_tr.innerHTML = "<td>"+key+"</td><td>"+aqiData[key]+"</td><td><button>删除</button></td>";
 		aqi_table.appendChild(aqi_tr);
 	}
 	
+	
 }
-
-
+btn_add.onclick = function(){
+	addAqiData();
+	// console.log(aqiData[0]);
+	renderAqiList();
+};
 
 /**
 * 点击add-btn时的处理逻辑
