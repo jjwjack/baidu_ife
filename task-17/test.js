8619a324300f27aa7b7d28974ae8c91cec082aa1
jjwjack <jjwjack@163.com>
2017-02-19 00:11:21 +0800

    task-17增加计算每周aqi。

--- task-17/test.js
+++ task-17/test.js
@@ -29,16 +29,16 @@ var aqiSourceData = {
 };
 //获取chart外框div
 var aqi_chart_wrap = document.getElementById("aqi_chart_wrap");
+//获取城市select标签
 var city_select = document.getElementById("city_select");
-
-// console.log(city_selected);
 var cities = ["北京", "天津", "上海", "广州"];
-//选择城市，画出图表
+
+//option的onchange事件，选择城市，画出图表
 city_select.onchange = function(){
 	aqi_chart_wrap.innerHTML = null;
 	var city_selected = city_select.options[city_select.selectedIndex].text;
-	chartDay(city_selected);
-	init();
+	drawDay(city_selected);
+	drawWeek(city_selected);
 };
 //生成日期和aqi,添加进aqiSourceData表
 function createSource(){
@@ -47,7 +47,7 @@ function createSource(){
 	}
 }
 createSource();
-// console.log(aqiSourceData["北京"]["2016-01-01"]);
+//随机生成aqi数值
 function createAqi(){
 	var myDate = [];
 	var myAqi = [];
@@ -59,9 +59,7 @@ function createAqi(){
 	}
 	return json_aqi;
 }
-
-// console.log(json_city);
-
+//随机生成颜色16进制值
 function createColor(){
 	//生成颜色
 	var colorNum = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];
@@ -81,26 +79,56 @@ function createColor(){
 	// console.log(strColor3);
 	
 }
-// createColor();
-function chartDay(city){
+//参数为城市名，画出每天的图表
+function drawDay(city){
 	var aqi_city = aqiSourceData[city];
-	console.log(aqi_city[0]);
 	var i_city = 0;
 	for(var key in aqiSourceData[city]){
 		var aqi_day = document.createElement("div");
-		// console.log(aqiSourceData["北京"][1]);
-		// aqi_day.innerHTML = null;
 		aqi_day.innerHTML = "<div style='width:10px; height:"+aqiSourceData[city][key]+"px; background-color: " + createColor() +"; position:absolute; bottom:0; left:"+(100+i_city*10)+"px;' title='123'>"
 		i_city += 1;
-		// aqi_day.innerHTML = "<div style='width:10px; height:111px; background-color: #f00; position:absolute; bottom:0; left:111px;' title='123'></div>";
-		// console.log(aqi_day);
 		aqi_chart_wrap.appendChild(aqi_day);
 	}
+}
+//参数为城市名，画出每周的图表
+function drawWeek(city){
+	var aqi_city = aqiSourceData[city];
+	var week1=week2=week3=week4=week5=0;
+	var i = 1;
+	for(var key in aqi_city){
+		// var aqi_week = document.createElement("div");
+		switch(true){
+			case i<=7:
+				week1 += aqi_city[key];
+				break;
+			case i<=14 :
+				week2 += aqi_city[key];
+				break;
+			case i<=21:
+				week3 += aqi_city[key];
+				break;
+			case i<=28:
+				week4 += aqi_city[key];
+				break;
+			case i<=32:
+				week5 += aqi_city[key];
+				break;
+			default:
+				console.log("week error");
+		}
+		i += 1;
+	}
+	console.log(week1);
+	console.log(week2);
+	console.log(week3);
+	console.log(week4);
+	console.log(week5);
 }
 
 
 function init(){
-	chartDay();
+	drawDay();
+	// drawWeek();
 }
 // init();
 
